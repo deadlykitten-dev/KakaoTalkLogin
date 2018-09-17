9165257743c198daa42c7a1ab1f2f965ef332dc9
sujang_yang <sjyang@bluefrog.co.kr>
2018-09-17 14:33:12 +0900

    Initial commit

---
+++ app/src/main/java/com/kestrel9/android/kakaotalklogin/common/GlobalApplication.java
@@ -0,0 +1,36 @@
+package com.kestrel9.android.kakaotalklogin.common;
+
+import android.app.Application;
+
+/**
+ * KakaoTalkLogin
+ * Class: GlobalApplication
+ * Created by kestr on 2018-09-17.
+ * <p>
+ * Description:
+ */
+public class GlobalApplication extends Application {
+
+    private static volatile GlobalApplication instance = null;
+
+    /**
+     * singleton 애플리케이션 객체를 얻는다.
+     * @return singleton 애플리케이션 객체
+     */
+
+    public static GlobalApplication getGlobalApplicationContext() {
+        if (instance == null)
+            throw new IllegalStateException("this application does not inherit com.kestrel9.android.kakaotalklogin.GlobalApplication");
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
